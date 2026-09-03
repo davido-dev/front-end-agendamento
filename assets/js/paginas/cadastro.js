9f32dbe7dac6c368bf3593f23e46d54df2673ba8
davido-dev <davido@prof.educacao.sp.gov.br>
2026-09-03 16:51:35 -0300

    03/09/2026

--- assets/js/paginas/cadastro.js
+++ assets/js/paginas/cadastro.js
@@ -1,63 +1,28 @@
-const form = document.querySelector('form');
-const cpfInput = document.querySelector('#cpf');
-const telefoneInput = document.querySelector("#telefone");
+const form = document.querySelector('form')
+const cpfInput = document.querySelector('#cpf')
+const telefone = document.querySelector('#telefone')
 
-// Cadastar usuário
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
 
-form.addEventListener('submit', function (event) {
-  event.preventDefault();
+    const nome = document.querySelector('#nome').value
+    const cpf = document.querySelector('#cpf').value
+    const email = document.querySelector('#email').value
+    const senha = document.querySelector('#senha').value
+    const confirmeSenha = document.querySelector('#confirmeSenha').value
+    const telefone = document.querySelector('#telefone').value
 
-  const nome = document.querySelector('#nome').value;
-  const cpf = document.querySelector('#cpf').value;
-  const email = document.querySelector('#email').value;
-  const senha = document.querySelector('#senha').value;
-  const confirmeSenha = document.querySelector('#confirmeSenha').value;
-  const telefone = document.querySelector('#telefone').value;
+    if (senha != confirmeSenha) {
+      alert("As senhas não são iguais")
+      return;
+    }
 
-  if (senha !== confirmeSenha) {
-    alert('As senhas não coincidem.');
+    const usuario = {
+      nome: nome,
+      cpf: cpf,
+      email: email,
+      senha: senha,
+      telefone: telefone
+    }
 
-    return;
-  }
-
-  const usuario = {
-    nome: nome,
-    cpf: cpf,
-    email: email,
-    senha: senha,
-    telefone: telefone,
-  };
-
-  alert(`${nome} cadastrado com sucesso!`)
-
-  // redirecionar para login
-});
-
-// Máscaras de inputs
-
-cpfInput.addEventListener('input', function () {
-  let cpf = cpfInput.value;
-
-  cpf = cpf.replace(/\D/g, '');
-
-  cpf = cpf.replace(/(\d{3})(\d)/, '$1.$2');
-  cpf = cpf.replace(/(\d{3})(\d)/, '$1.$2');
-  cpf = cpf.replace(/(\d{3})(\d{1,2})$/, '$1-$2');
-
-  cpfInput.value = cpf;
-});
-
-
-telefoneInput.addEventListener("input", function () {
-
-  let telefone = telefoneInput.value;
-
-  telefone = telefone.replace(/\D/g, "");
-
-  telefone = telefone.replace(/(\d{2})(\d)/, "($1) $2");
-
-  telefone = telefone.replace(/(\d{5})(\d)/, "$1-$2");
-
-  telefoneInput.value = telefone;
-
-});
+})
